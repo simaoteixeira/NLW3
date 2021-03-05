887554f0a7da59d6e9b27e25e399d4aa706ae754
simaotex <simaotexeira15@gmail.com>
2021-03-05 19:08:38 +0000

    commit backend final

---
+++ backend/src/views/images_view.ts
@@ -0,0 +1,13 @@
+import Image from '../models/Image';
+
+export default {
+    render(image: Image) {
+        return {
+            id: image.id,
+            url: `http://192.168.1.10:3333/uploads/${image.path}`
+        };
+    },
+    renderMany (image: Image[]) {
+        return image.map(image => this.render(image));
+    }
+};
